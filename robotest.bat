d1e24e3912ac156ebb3613c2e2ea47bdc03103c9
Thomas Kolf <thk9051@thi.de>
2026-08-07 12:06:19 +0200

    add test robocopy

--- robotest.bat
+++ robotest.bat
@@ -1,40 +1,49 @@
-@echo off
-setlocal
+Microsoft Windows [Version 10.0.17763.8880]
+(c) 2018 Microsoft Corporation. All rights reserved.
 
-REM ==============================
-REM Robocopy standalone test
-REM ==============================
+C:\Processing>robotest.bat
+================================================
+ROBocopy TEST
+Source:      V:\
+Destination: \\vt1.vitesco.com\SMT\didv0776\DataTransfer\Keyence_VR5200_Data
+Log:         C:\Processing\robocopy_test.txt
+================================================
 
-set "SRC=V:\"
-set "DST=\\vt1.vitesco.com\SMT\didv0776\DataTransfer\Keyence_VR5200_Data"
-set "LOG=%~dp0robocopy_test.txt"
 
-echo ================================================
-echo ROBocopy TEST
-echo Source:      %SRC%
-echo Destination: %DST%
-echo Log:         %LOG%
-echo ================================================
-echo.
+-------------------------------------------------------------------------------
+   ROBOCOPY     ::     Robust File Copy for Windows
+-------------------------------------------------------------------------------
 
-robocopy "%SRC%" "%DST%" /S /COPY:DAT /DCOPY:T /R:2 /W:2 /XJ /XD "System Volume Information" "$RECYCLE.BIN" /LOG+:"%LOG%" /TEE
+  Started : Freitag, 7. August 2026 12:02:38
+   Source - V:\" \vt1.vitesco.com\SMT\didv0776\DataTransfer\Keyence_VR5200_Data \S \COPY:DAT \DCOPY:T \R:2 \W:2 \XJ \XD System\
+     Dest - C:\Processing\Volume\
 
-set "ROBO_EXIT=%ERRORLEVEL%"
+    Files :
+  Options : /DCOPY:DA /COPY:DAT /R:1000000 /W:30
 
-echo.
-echo ================================================
-echo ROBOCOPY EXIT CODE: %ROBO_EXIT%
-echo ================================================
+------------------------------------------------------------------------------
 
-if %ROBO_EXIT% LEQ 7 (
-    echo Robocopy completed successfully.
-) else (
-    echo Robocopy FAILED.
-)
+ERROR : Invalid Parameter #3 : "Information $RECYCLE.BIN /LOG+:C:\Processing\robocopy_test.txt /TEE"
 
-echo.
-echo Check the detailed log:
-echo %LOG%
-echo.
+       Simple Usage :: ROBOCOPY source destination /MIR
 
-pause
+             source :: Source Directory (drive:\path or \\server\share\path).
+        destination :: Destination Dir  (drive:\path or \\server\share\path).
+               /MIR :: Mirror a complete directory tree.
+
+    For more usage information run ROBOCOPY /?
+
+
+****  /MIR can DELETE files as well as copy them !
+
+================================================
+ROBOCOPY EXIT CODE: 16
+================================================
+Robocopy FAILED.
+
+Check the detailed log:
+C:\Processing\robocopy_test.txt
+
+Press any key to continue . . .
+
+C:\Processing>
